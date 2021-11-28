c9365c6d52a977f9fc361de8a13cc36f9758d57b
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 16:35:03 -0300

    teste

--- index.php
+++ index.php
@@ -8,15 +8,9 @@
 <link href="css/estilo.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
 <header>
-    <img class="logo" src="/imagens/logo.png">
+    
   <nav>
-      
-    <ul>
-        
-        <li><a href="#" class="current">Home</a></li>
-        <li><a href="#">Categorias</a></li>
-        <li><a href="https://venddiapi.herokuapp.com/desenvolvimento/rotasparaimportacao" >Atualizar Rota</a></li>
-    </ul>
+    <img class="logo" src="/imagens/logo.png">
     
   </nav>
 </header>
