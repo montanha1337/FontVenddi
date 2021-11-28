39ac5726023403cb90ece1bfd5d5389f4ecd1812
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 16:56:46 -0300

    teste

--- index.php
+++ index.php
@@ -12,5 +12,12 @@
   <nav>
     <img class="logo" src="/imagens/logo.png">
     <li><a href="#" class="current">Home</a></li>
+    <li><a href="https://venddiapi.herokuapp.com/desenvolvimento/rotasparaimportacao" >Atualizar Rota</a></li>
   </nav>
 </header>
+<body>
+ <h1>Aqui jas um corpo</h1>
+</body>
+<script>
+    
+</script>
