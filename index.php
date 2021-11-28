9b845dff2bad7935b5a49fd4379827ab65bc7fcf
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 16:28:32 -0300

    posicionando logo

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
 <link href="css/estilo.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
 <header>
+    <img class="logo" src="/imagens/logo.png">
   <nav>
       
     <ul>
-        <img class="logo" src="/imagens/logo.png">
+        
         <li><a href="#" class="current">Home</a></li>
         <li><a href="#">Categorias</a></li>
         <li><a href="https://venddiapi.herokuapp.com/desenvolvimento/rotasparaimportacao" >Atualizar Rota</a></li>
