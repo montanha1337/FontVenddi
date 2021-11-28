16afebc7ed3dadb47822407c4c32a0dbe63b690d
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 19:48:18 -0300

    teste

--- js/funcoes.js
+++ js/funcoes.js
@@ -2,9 +2,8 @@ var Conexao = import("./conexao.js")
 window.addEventListener("load",myFunction);
 
 
-
 function myFunction() {
-    const texto =Conexao.texto
+    const texto =Conexao.texto()
     console.log(texto)
   document.getElementById("demo").textContent=texto;
   document.getElementById("teste").textContent=texto;       
