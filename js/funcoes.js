78dda32c67ae10fa7326f04c8a51090fe23eabf0
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 19:00:30 -0300

    teste

--- js/funcoes.js
+++ js/funcoes.js
@@ -11,5 +11,8 @@ function myFunctionNew() {
     document.getElementById("demo").textContent="mudou demo";  
     document.getElementById("teste").textContent="mudou teste";       
   }
+ function soma(){
+     
+ }
 
  module.exports = {myFunction,myFunctionNew}
