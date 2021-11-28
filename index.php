91c6ccae36b46ee54261c45cc2f4f83885cdd8f9
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 16:32:26 -0300

    posição logo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8"/>
 <title>Vendi Api</title>
+<link rel="shortcut icon" href="imagens\SomenteIconAzul.ico">
 </head>
 <link href="css/estilo.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
