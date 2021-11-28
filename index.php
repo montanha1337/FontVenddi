b5a3992febc79d36b8f0fdc777ac3c7ad37e8fde
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 16:15:45 -0300

    correção estilo

--- index.php
+++ index.php
@@ -4,28 +4,19 @@
 <meta charset="UTF-8"/>
 <title>Vendi Api</title>
 </head>
-<link href="css/estilos.css" rel="stylesheet">
+<link href="css/estilo.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
 <header>
   <nav>
       <a href="#" id="responsive-menu"></a>
     <ul>
-      <li><a href="#" class="current">Home</a></li>
-      <li><a href="#">Categorias</a></li>
-      <li><a href="https://venddiapi.herokuapp.com/desenvolvimento/rotasparaimportacao" >Atualizar Rota</a></li>
+        <img src="/imagens/logo.png">
+        <li><a href="#" class="current">Home</a></li>
+        <li><a href="#">Categorias</a></li>
+        <li><a href="https://venddiapi.herokuapp.com/desenvolvimento/rotasparaimportacao" >Atualizar Rota</a></li>
     </ul>
     
   </nav>
 </header>
-<div class="container">
-   <div class="box">
-    <div class="animation">
-     <div class="one spin-one"></div>
-     <div class="two spin-two"></div>
-     <div class="three spin-one"></div>
-    </div>
-  <h1>Vendi Api</h1><br>
-  <p>Desculpe, Tente reenviar seu pedido.</p
-  
-  </div>
+
 </div>
