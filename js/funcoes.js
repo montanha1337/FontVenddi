8efd7bcd8370a4cd4360c4fe61c36d2e2b0342ac
montanha1337 <matheusdteixeira@hotmail.com>
2021-11-28 18:16:42 -0300

    teste

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,5 @@
 function myFunction() {
-  document.getElementById("demo").style.fontSize = "25px"; 
-  document.getElementById("demo").style.color = "red";
-  document.getElementById("demo").style.backgroundColor = "yellow";
+
   document.getElementById("demo").textContent="Aprendendo criar elementos"        
 }
  module.exports = {myFunction}
